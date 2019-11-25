87a8d41bcbf647c475553eef7f3c7274a3bf366b
Alyssa Kennedy <kennedy.alyssa@gmail.com>
2019-11-25 13:35:54 -0500

    initial files for project

---
+++ src/main/java/org/launchcode/Lokker/App/LokkerAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Lokker.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokkerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokkerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Lokker/App/LokkerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Lokker.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokkerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
